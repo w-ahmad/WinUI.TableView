ccce965253ce77de550db8186b065e459b9e7bcd
Waheed Ahmad <ewammuslim@outlook.com>
2024-12-16 03:49:43 +0500

    added cell and row context flyouts

---
+++ src/WinUI.TableView/TableViewRowContextFlyoutEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls.Primitives;
+using System.ComponentModel;
+
+namespace WinUI.TableView;
+
+public partial class TableViewRowContextFlyoutEventArgs : HandledEventArgs
+{
+    public TableViewRowContextFlyoutEventArgs(int index, TableViewRow row, object item, FlyoutBase flyoutBase)
+    {
+        Index = index;
+        Row = row;
+        Item = item;
+        Flyout = flyoutBase;
+    }
+
+    public int Index { get; }
+    public TableViewRow Row { get; }
+    public object Item { get; }
+    public FlyoutBase Flyout { get; }
+}
