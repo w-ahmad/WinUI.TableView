738a00a445223b80d08d3408be6123b209a2c324
Waheed Ahmad <waheed.ahmad92@outlook.com>
2025-11-18 17:15:32 +0500

    Revert "Limited selection checkbox movement to VisibleWhenSelected.  Note there's still a a bug if switching from VisibleWhenSelected to VisibleWhenExpanded, but I don't understand TableViewRowPresenter's animations enough to confidently fix it without worrying about breaking something."
    This reverts commit 11b7c329bf9d993c47e3a97fa5e90ddf0204018b.

--- src/TableViewRow.cs
+++ src/TableViewRow.cs
@@ -506,9 +506,7 @@ public partial class TableViewRow : ListViewItem
             selectionIndicator = fontIcon?.Parent as Border;
         }
 
-        if (selectionIndicator is not null 
-                    &&TableView is not null
-                    && TableView.RowDetailsVisibilityMode == TableViewRowDetailsVisibilityMode.VisibleWhenSelected)
+        if (selectionIndicator is not null)
         {
             if (IsSelected)
             {
@@ -534,8 +532,7 @@ public partial class TableViewRow : ListViewItem
             else
             {
                 // Row is not selected → reset any previous offset
-                if (selectionIndicator.RenderTransform is TranslateTransform tt
-                     )
+                if (selectionIndicator.RenderTransform is TranslateTransform tt)
                     tt.Y = 0;
                 else
                     selectionIndicator.RenderTransform = null;
