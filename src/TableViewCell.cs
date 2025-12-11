155d197f42d2226027253b00184156d712f6b911
Waheed Ahmad <waheed.ahmad92@outlook.com>
2025-12-12 01:23:25 +0500

    fix indentation

--- src/TableViewCell.cs
+++ src/TableViewCell.cs
@@ -448,7 +448,7 @@ public partial class TableViewCell : ContentControl
             element.Style = boundColumn.ElementStyle;
         }
 
-            Content = element;
+        Content = element;
 
 #if !WINDOWS
         DispatcherQueue.TryEnqueue(async () =>
