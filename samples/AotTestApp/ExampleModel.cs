d70078f106b7be1a8dcefe35c21fed1a2e422222
Waheed Ahmad <waheed.ahmad92@outlook.com>
2026-04-17 17:00:18 +0500

    Add Aot test project

---
+++ samples/AotTestApp/ExampleModel.cs
@@ -0,0 +1,73 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AotTestApp;
+
+public partial class Designation : ObservableObject
+{
+    public Designation(int id, string? title)
+    {
+        Id = id;
+        Title = title;
+    }
+
+    [ObservableProperty]
+    public partial int Id { get; set; }
+
+
+    [ObservableProperty]
+    public partial string? Title { get; set; }
+}
+
+public partial class User : ObservableObject
+{
+    [ObservableProperty]
+    [Display(ShortName = "First Name")]
+    public partial string? FirstName { get; set; }
+
+    [ObservableProperty]
+    [Display(ShortName = "Last Name")]
+    public partial string? LastName { get; set; }
+
+    [ObservableProperty]
+    public partial string? Email { get; set; }
+
+    [ObservableProperty]
+    public partial string? Gender { get; set; }
+
+    [ObservableProperty]
+    public partial DateOnly Dob { get; set; }
+}
+
+public partial class ExampleModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial int Id { get; set; }
+
+    [ObservableProperty]
+    [Display(ShortName = "Active At")]
+    public partial TimeOnly ActiveAt { get; set; }
+
+    [ObservableProperty]
+    [Display(ShortName = "Is Active")]
+    public partial bool IsActive { get; set; }
+
+    [ObservableProperty]
+    public partial string? Department { get; set; }
+
+    [ObservableProperty]
+    public partial Designation? Designation { get; set; }
+
+    [ObservableProperty]
+    public partial string? Address { get; set; }
+
+    [ObservableProperty]
+    [Display(AutoGenerateField = false)]
+    public partial string? Avatar { get; set; }
+
+    public Uri AvatarUrl => new(Avatar ?? string.Empty);
+
+    [ObservableProperty]
+    [Display(AutoGenerateField = false)]
+    public partial User? User { get; set; }
+}
---
+++ samples/AotTestApp/MainViewModel.cs
@@ -0,0 +1,72 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AotTestApp;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        foreach (var item in ItemsList)
+        {
+            Items.Add(item);
+            Genders.Add(item.User?.Gender);
+            Departments.Add(item.Department);
+        }
+
+        Designations = [.. DataFaker.JobTitles];
+    }
+
+    public static void InitializeItems()
+    {
+        var startId = 1;
+        var startDate = new DateOnly(1970, 1, 1);
+
+        ItemsList.Clear();
+
+        for (var i = 0; i < 1_000; i++)
+        {
+            var firstName = DataFaker.FirstName();
+            var lastName = DataFaker.LastName();
+            var email = DataFaker.Email(firstName, lastName);
+            var gender = DataFaker.Gender();
+            var dob = DataFaker.PastDate(50, startDate);
+            ItemsList.Add(new ExampleModel
+            {
+                Id = startId++,
+                IsActive = DataFaker.Boolean(),
+                ActiveAt = DataFaker.TimeOfDay(),
+                Department = DataFaker.Department(),
+                Designation = DataFaker.JobTitle(),
+                Address = DataFaker.Address(),
+                Avatar = DataFaker.Avatar(),
+                User = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    Gender = gender,
+                    Dob = dob
+                }
+            });
+        }
+    }
+
+    public static List<ExampleModel> ItemsList { get; } = [];
+
+    [ObservableProperty]
+    public partial ObservableCollection<ExampleModel> Items { get; set; } = [];
+
+    public IList<string?> Genders { get; set; } = [];
+
+    public IList<string?> Departments { get; set; } = [];
+
+    public IList<Designation> Designations { get; set; } = [];
+
+    [ObservableProperty]
+    public partial ExampleModel? SelectedItem { get; set; }
+}
+
